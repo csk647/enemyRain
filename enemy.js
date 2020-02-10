5eb05335f17b1469f39572ff657a72d6c75e10c6
aeri <csk647@gmail.com>
2020-02-10 23:22:54 +0900

    clearInterval써보기

--- enemy.js
+++ enemy.js
@@ -7,24 +7,11 @@ class ghost{
      constructor(){
     //     const randomX = Math.floor(Math.random() * 765) + 1;
     //     // 이게 밖에 있으면 랜덤 숫자가 한개만 나오고 귀신은 여러개 나온다. 이게 안에 있어야 계속 새로운 숫자가 나온다.
-     
-    
-       
-       
-   
-       
-    //     setInterval(
-    //        (this.fallingGhost)
-    //     ,30);
-    //     ghost.numInstances = (ghost.numInstances || 0) + 1;
-          }
-       
-    
     
 // 왜 아이디에는 따옴표가 들어가면 안돼?
-    
+     }
     makeGhost = () => {
-        if(howMany.length < 10){
+        if(howMany.length < 1){
             // 클래스로 귀신 불러서 .length 이용해서 귀신 갯수 관리하기
     const randomX = Math.floor(Math.random() * 765) + 1;
     // 이게 밖에 있으면 랜덤 숫자가 한개만 나오고 귀신은 여러개 나온다. 이게 안에 있어야 계속 새로운 숫자가 나온다.
@@ -40,19 +27,25 @@ class ghost{
 
     // console.log(typeof this.ghost.offsetTop);
  
-    setInterval(
+    this.movement = setInterval(
         (this.fallingGhost)
         , 30);
+    // 변수에 setInterval을 넣으면 다른곳에서 호출할 때 힘드니까 this. 로 넣어준다.
+    // this.movement 는 영수증 같은 역할을 한다. 이제 이 this변수라고 해야하나 그걸 clearInterval로 이용할 수 있다.
 }
     }
-
-
+     
+   
     fallingGhost = () => {
         bg.appendChild(this.ghost);
         this.ghost.style.top = parseInt(this.ghost.style.top) + 2+'px';
-
         if( parseInt(this.ghost.style.top) > 550){
+            // this.ghost.parentNode.removeChild(this.ghost)
             this.ghost.parentNode.removeChild(this.ghost)
+            clearInterval(this.movement)
+            console.log(this.ghost.style.top)
+
+
         //  이거 두 줄로 귀신 없애는 것 만들었다
         }
         
@@ -62,7 +55,6 @@ class ghost{
 
         if(ghostHead === 494  && this.ghost.offsetLeft < heroRight 
             && this.ghost.offsetLeft > heroLeft ){
-            console.log("hi")
             this.ghost.classList.add('dieGhost')
             } 
         // hero의 범위를 정해줘서 그 범위 내에 귀신이 들어오면 사라지는 모습으로 변하도록
@@ -72,15 +64,15 @@ class ghost{
 
 
 
-function last() {
+ function last() {
     
-    const cetsSee = setInterval(function(){
+    let container = setInterval(function(){
     let gNew = new ghost();
     gNew.makeGhost();
     } ,3000);
 
-}
-last();
+ }
+ last();
 
 // offset값->offset은 왜 둘 다 쓰면 안되고 오른쪽에만 쓸 수 있는 거지...!?
 // , computed, computed 안쓸 때 style의 top값을 지정해줘야 한다는 것...?
